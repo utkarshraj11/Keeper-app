a750cca10d0339b7f5df1f925d11d8fea0a21acb
utkarsh raj <utkarsh.alok26@gmail.com>
2020-04-13 16:08:45 +0530

    Added notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes.js";
+
 
 function App() {
+  
+  // eslint-disable-next-line
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
